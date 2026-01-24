cbb08e50a19ee7449595ace115a0cb67454ebdd0
Yash Haritash <yash.haritash@unifyapps.com>
2026-01-24 14:40:46 +0530

    fix

--- src/app/table/page.tsx
+++ src/app/table/page.tsx
@@ -5,6 +5,7 @@ import {
     useMaterialReactTable,
     type MRT_ColumnDef,
 } from 'material-react-table';
+import GoBackButton from '@/components/GoBackButton';
 
 //example data type
 type Person = {
@@ -104,7 +105,21 @@ const Example = () => {
         data, //data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
     });
 
-    return <MaterialReactTable table={table} />;
+    return (
+        <>
+            <div className="flex justify-between items-center mb-3 px-4 py-2">
+                <div className="text-white flex items-center gap-3">
+
+                    <GoBackButton />
+
+                </div>
+            </div>
+            <div className='flex h-full w-full flex-col items-center p-2 justify-center'>
+                <MaterialReactTable table={table} />
+            </div>
+        </>
+
+    );
 };
 
 export default Example;
